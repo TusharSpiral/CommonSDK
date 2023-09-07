c331857e20dd4e51ca089e15801d26fe6114f574
Tushar Goyal <tushargoyal@remotestate.com>
2023-09-07 16:19:45 +0530

    Add Framework

---
+++ CommonSDK/CommonSDK/CommonSDK.h
@@ -0,0 +1,18 @@
+//
+//  CommonSDK.h
+//  CommonSDK
+//
+//  Created by Tushar Goyal on 07/09/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonSDK.
+FOUNDATION_EXPORT double CommonSDKVersionNumber;
+
+//! Project version string for CommonSDK.
+FOUNDATION_EXPORT const unsigned char CommonSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonSDK/PublicHeader.h>
+
+
